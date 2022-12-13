f8e6fc78f4c3fc80c6e2e59c7e0d18a98358a620
Paraskalyan001 <88899894+Paraskalyan001@users.noreply.github.com>
2022-12-13 19:46:28 +0530

    added UI and toss logic
    // line 120  Toss event interaction
    ISSUE !! line 127 ui option promt  -
    loop is not waiting for UI input

--- src/game.ts
+++ src/game.ts
@@ -1,78 +1,204 @@
-let base = new Entity()
+import * as ui from "@dcl/ui-scene-utils";
 
-base.addComponent(new GLTFShape("models/Theatre.glb"))
-base.addComponent(new Transform({
-    position: new Vector3(8,0,8),
-    rotation: Quaternion.Euler(0,-90,0),
-}))
-engine.addEntity(base)
+let base = new Entity();
 
+base.addComponent(new GLTFShape("models/Theatre.glb"));
+base.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 8),
+    rotation: Quaternion.Euler(0, -90, 0),
+  })
+);
+engine.addEntity(base);
 
-let table = new Entity()
-let coin = new Entity()
+//----------------------------------
 
-engine.addEntity(table)
-engine.addEntity(coin)
+let cylinder = new CylinderShape();
 
-coin.addComponent(new GLTFShape("models/coin_withTossAnim.glb"))
-table.addComponent(new GLTFShape("models/Lp_wc_As_table.glb"))
+let wheel1 = new Entity();
+wheel1.addComponent(cylinder);
+wheel1.addComponent(
+  new Transform({
+    position: new Vector3(6, 2, 11.9),
+    rotation: Quaternion.Euler(90, 0, 0),
+    scale: new Vector3(1, 0.05, 1),
+  })
+);
 
-coin.addComponent(new Transform({
-    position: new Vector3(8,0,4),
+engine.addEntity(wheel1);
+
+let wheel2 = new Entity();
+wheel2.addComponent(cylinder);
+wheel2.addComponent(
+  new Transform({
+    position: new Vector3(10, 2, 11.9),
+    rotation: Quaternion.Euler(90, 0, 0),
+    scale: new Vector3(1, 0.05, 1),
+  })
+);
+
+engine.addEntity(wheel2);
+
+let mymaterial = new Material();
+mymaterial.albedoTexture = new Texture("materials/hypno-wheel.png");
+
+wheel1.addComponent(mymaterial);
+wheel2.addComponent(mymaterial);
+
+wheel1.addComponent(
+  new OnPointerDown(
+    (e) => {
+      log("wiii");
+      new ui.OptionPrompt(
+        "Pick an option!",
+        "What will you choose?",
+        () => {
+          log(`picked option A`);
+        },
+        () => {
+          log(`picked option B`);
+        },
+        "Pick A",
+        "Pick B"
+      );
+    },
+    { button: ActionButton.POINTER, hoverText: "spin" }
+  )
+);
+
+wheel2.addComponent(
+  new OnPointerDown(
+    (e) => {
+      new ui.OptionPrompt(
+        "Pick an option!",
+        "What will you choose?",
+        () => {
+          log(`picked option A`);
+        },
+        () => {
+          log(`picked option B`);
+        },
+        "Pick A",
+        "Pick B"
+      );
+      log("wiii");
+    },
+    { button: ActionButton.POINTER, hoverText: "spin" }
+  )
+);
+
+//-------------------------------------
+
+let table = new Entity();
+let coin = new Entity();
+
+engine.addEntity(table);
+engine.addEntity(coin);
+
+coin.addComponent(new GLTFShape("models/coin_withTossAnim.glb"));
+table.addComponent(new GLTFShape("models/Lp_wc_As_table.glb"));
+
+coin.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 4),
     // rotation: Quaternion.Euler(0,0,0),
-}))
-table.addComponent(new Transform({
-    position: new Vector3(8,0,4),
-    // rotation: Quaternion.Euler(0,0,0),
-}))
-coin.addComponent(new Animator())
-coin.getComponent(Animator).addClip(new AnimationState("Toss", {looping: false}))
-table.addComponent(new AudioSource(new AudioClip("sounds/coin_toss_audio.mp3")))
+  })
+);
 table.addComponent(
-    new OnClick((): void => {
+  new Transform({
+    position: new Vector3(8, 0, 4),
+    // rotation: Quaternion.Euler(0,0,0),
+  })
+);
+coin.addComponent(new Animator());
+coin
+  .getComponent(Animator)
+  .addClip(new AnimationState("Toss", { looping: false }));
+table.addComponent(
+  new AudioSource(new AudioClip("sounds/coin_toss_audio.mp3"))
+);
+table.addComponent(
+  new OnPointerDown(
+    (e) => {
+      log("toss event started");
+      let sum: number = 0;
+      let j: number = 0;
+      for (let i = 0; i < 11; i++) {
+        new ui.OptionPrompt(
+          "Toss!",
+          "choose a side",
+          () => {
+            log(`picked Tails`);
+            //   j = 0;
+          },
+          () => {
+            log(`picked heads`);
+            //   j = 1;
+          },
+          "Tails",
+          "Heads"
+        );
         coin.getComponent(Animator).getClip("Toss").play();
         table.getComponent(AudioSource).playOnce();
-        log("toss");
-    })  
-)
+        sum += subToss(j);
+      }
+
+      if (sum >= 6) {
+        log("win");
+        new ui.OkPrompt(
+          "Final Results : WON",
+          () => {
+            log(`store data here`);
+          },
+          "Next",
+          true
+        );
+        // return 1;
+      } else {
+        log("loss");
+        new ui.OkPrompt(
+          "Final Results : LOST",
+          () => {
+            log(`store data here`);
+          },
+          "Next",
+          true
+        );
+        // return 0;
+      }
+    },
+    { button: ActionButton.POINTER, hoverText: "Toss" }
+  )
+);
 
 
-
-
-
-
-function tossEvent(){
-    const tossResults: number[] = new Array(11); 
-    for (let i = 0; i < tossResults.length; i++) {
-        const j = 0; // we will take j as input here using ui
-        tossResults[i]  = subToss(j);
-    }
-
-    let sum:number = 0;
-    for (let i = 0; i < tossResults.length; i++) {
-        sum += tossResults[i];
-    }
-
-    if (sum >= 6) {
-        log("win")
-        return 1;
-    }
-    else{
-        log("loss")
-        return 0;
-    }
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
 }
 
-function getRandomInt(max:number) {
-    return Math.floor(Math.random() * max);
+function subToss(p1: number) {
+  let r: Number = getRandomInt(2);
+  if (r == p1) {
+    new ui.OkPrompt(
+      "You won ",
+      () => {
+        log(`Correct prediction`);
+      },
+      "Next",
+      true
+    );
+    return 0;
+  } else {
+    new ui.OkPrompt(
+      "You Lost ",
+      () => {
+        log(`Wrong prediction`);
+      },
+      "Next",
+      true
+    );
+    return 1;
+  }
 }
 
-function subToss(p1:number) {
-    let r:Number = getRandomInt(2);
-    if (r == p1) {
-        return 0;
-    }
-    else{
-        return 1;
-    }
-}
+// engine.addSystem(tossEvent)
